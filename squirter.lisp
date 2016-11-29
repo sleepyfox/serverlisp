c9b6b29edd44702968693eb0f21c18a2dadfc82e
Nigel Runnels-Moss <sleepyfox@gmail.com>
2016-11-29 22:33:28 +0000

    remove extraneous message bus implementations from squirter

--- squirter.lisp
+++ squirter.lisp
@@ -1,25 +1,9 @@
 ;; Imports
-(var AWS (require "aws-sdk"))
-(AWS.config.update { region: "us-east-1" })
-(var sns (new AWS.SNS))
-(var R (require "rsmq"))
-(var rsmq (new R))
+(var R (require "rsmq")
+     rsmq (new R))
 
 ;; Constants
 (var TIME_DELAY (or process.env.DELAY 5000))
-
-;; Send a message to AWS SNS
-(def send-sns-message (json)
-     (var params {
-          Message: (JSON.stringify json)
-          MessageStructure: "json"
-          Subject: "A message for lottie"
-          TopicArn: "arn:aws:sns:us-east-1:859676677036:test-topic" })
-
-     (sns.publish params (# (err, data)
-                            (ternary err
-                                     (console.log err err.stack)
-                                     (console.log "sns:" data)))))
 
 ;; Send a message to a local Redis Message Queue
 (def send-rsmq-message (m)
@@ -27,8 +11,7 @@
                         (# (err, response)
                            (ternary err
                                     (console.log "error" err)
-                                    (console.log "message" response))
-                           (process.exit))))
+                                    (console.log "message" response)))))
      
 ;; Returns a random number from 0 (inclusive) to n (exclusive)
 (def random (n)
@@ -48,9 +31,9 @@
 
 ;; Given a venue return a random course
 (def choose-course (venue)
-     (if (= venue "The Belfry")
-         (sample ["Brabazon" "PGA National" "Derby"])
-         (sample ["Roman Road" "Montgomerie" "Twenty Ten"])))
+     (ternary (= venue "The Belfry")
+              (sample ["Brabazon" "PGA National" "Derby"])
+              (sample ["Roman Road" "Montgomerie" "Twenty Ten"])))
 
 ;; Returns a random number between 1 and 4
 (def choose-pax ()
@@ -65,20 +48,10 @@
        course: (choose-course venue)
        pax: (choose-pax)})
 
-;; Sends a message via the message bus
-(def send-message (sale)
-     (console.log (JSON.stringify sale null 2))
-     (.sendMessage eventHubs {
-                   message: sale
-                   deviceId: 1}))
-
 ;; sends a new message every n seconds
 (def create-data-loop ()
      (set-timeout (#>
-                   (console.log "status" (send-message (create-new-sale)))
+                   (send-rsmq-message (create-new-sale))
                    (create-data-loop)) TIME_DELAY))
 
-;;(create-data-loop)
-;;(send-sns-message (create-new-sale))   
-(send-rsmq-message (create-new-sale))
-
+(create-data-loop)
